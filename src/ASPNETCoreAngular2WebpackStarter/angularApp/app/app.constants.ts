75d5c3d67382f43d88221ad57097d3a1fa91ac20
Aleksey Rublevsky <a.rublevsky@itransition.com>
2017-02-20 18:43:51 +0300

    refactoring

--- src/ASPNETCoreAngular2WebpackStarter/angularApp/app/app.constants.ts
+++ src/ASPNETCoreAngular2WebpackStarter/angularApp/app/app.constants.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class Configuration {
-    public Server: string = (process.env.NODE_ENV || "development").trim() === 'development' ? 'localhost:3000' : 'http://arublevsky-001-site1.btempurl.com';
+    public Server: string = 'http://arublevsky-001-site1.btempurl.com/';
     public ApiUrl: string = 'api/';
     public ServerWithApiUrl = this.Server + this.ApiUrl;
 }
